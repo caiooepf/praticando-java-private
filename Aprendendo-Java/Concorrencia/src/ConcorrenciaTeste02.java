629dc696a694f212baef63c26365922bbf701d1c
Caio Eduardo <caiotech02@gmail.com>
2026-01-08 03:01:34 -0300

    Continuar do topico Yeild e Join

---
+++ Aprendendo-Java/Concorrencia/src/ConcorrenciaTeste02.java
@@ -0,0 +1,44 @@
+
+    class ThreadExample2 extends  Thread{
+    private char c;
+
+    public ThreadExample2(char c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 100 ; i++) {
+            System.out.print(c);
+            if (i % 100 == 0){
+                System.out.println();
+            }
+        }
+    }
+}
+
+    class ThreadRunnable2 implements Runnable {
+    private char c;
+        public ThreadRunnable2 (char c) {
+            this.c = c;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName());
+            for (int i = 0; i < 100 ; i++) {
+                System.out.print(c);
+                if (i % 100 == 0){
+                    System.out.println();
+                }
+            }
+        }
+    }
+public class ConcorrenciaTeste02 {
+
+
+    static void main(String[] args) {
+
+    }
+}
